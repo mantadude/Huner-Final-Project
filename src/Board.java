b02a307d612639c123ff0867a9d240f2c09678e6
David Huner <dhuner@ltsp194.udallas.edu>
2019-12-03 13:28:43 -0600

    commit

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,4 @@
-import java.io.*;
+import java.util.ArrayList;
 
 
 public class Board {
@@ -13,7 +13,21 @@ public void BlankBoard(int[] a) {
 	a[6] = 0;
 	a[13] = 0;
 }
+
+public void BoardPrinter(int [] Rocks) {
+	String line1 = "-----------------";
+	String line2 = "| |" + Rocks[12] + "|" + Rocks[11] + "|" + Rocks[10] + "|" + Rocks[9] + "|" + Rocks[8] + "|" + Rocks[7] + "| |";
+	String line3 = "|" + Rocks[13] + "-------------" + Rocks[6] + "|";
+	String line4 = "| |" + Rocks[0] + "|" + Rocks[1] + "|" + Rocks[2] + "|" + Rocks[3] + "|" + Rocks[4] + "|" + Rocks[5] + "| |";
+	String line5 = "-----------------";
+	String line6 = "   " + 0 + " " + 1 + " " + 2 + " " + 3 + " " + 4 + " " + 5 + "   ";
+	
+	String CompleteBoard = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5 + "\n" + line6; 
+	System.out.print(CompleteBoard);
 }
+}
+
+
 	
 	
 
